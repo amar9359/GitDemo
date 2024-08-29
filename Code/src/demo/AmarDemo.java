0be9e5a0412448e1fe9b34164a0c8ea6c768a91e
Amar <amarkadam7240@gmail.com>
2024-08-29 11:27:39 +0530

    Deleted some files

--- Code/src/demo/AmarDemo.java
+++ Code/src/demo/AmarDemo.java
@@ -3,10 +3,7 @@ package demo;
 public class AmarDemo {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello world");
-		System.out.println("Hiiiiiiiiiiiiiii");
 		System.out.println("Project upload branch A");
-		System.out.println("-------------------------------");
 		System.out.println("Branch b");
 		System.out.println("-------------------============");
 		
