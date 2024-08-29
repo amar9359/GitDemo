33a56ba468711f47127a5004c52f99d9af46f761
Amar <amarkadam7240@gmail.com>
2024-08-29 11:32:00 +0530

    Branch B1 changes

--- Code/src/demo/AmarDemo.java
+++ Code/src/demo/AmarDemo.java
@@ -3,13 +3,9 @@ package demo;
 public class AmarDemo {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello world");
-		System.out.println("Hiiiiiiiiiiiiiii");
-		System.out.println("Project upload branch A");
 		System.out.println("-------------------------------");
 		System.out.println("Branch b");
 		System.out.println("-------------------============");
-		
 	}
 
 }
