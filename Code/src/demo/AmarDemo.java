2183b4cf4d421e8fea9fe07b5d1f9a63e441ed99
Amar <amarkadam7240@gmail.com>
2024-08-29 11:05:55 +0530

    Branch BB

--- Code/src/demo/AmarDemo.java
+++ Code/src/demo/AmarDemo.java
@@ -5,13 +5,10 @@ public class AmarDemo {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("Hiiiiiiiiiiiiiii");
-<<<<<<< Updated upstream
 		System.out.println("Project upload branch A");
 		System.out.println("-------------------------------");
-=======
 		System.out.println("Branch b");
 		System.out.println("-------------------============");
->>>>>>> Stashed changes
 	}
 
 }
