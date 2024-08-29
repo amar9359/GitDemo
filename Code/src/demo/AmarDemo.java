3f4d5ae78bc839eba1b27c5f5ad88db84e42d0f5
Amar <amarkadam7240@gmail.com>
2024-08-29 11:34:08 +0530

    Branch B1 changes

--- Code/src/demo/AmarDemo.java
+++ Code/src/demo/AmarDemo.java
@@ -4,7 +4,7 @@ public class AmarDemo {
 	
 	public static void main(String[] args) {
 		System.out.println("-------------------------------");
-		System.out.println("Branch b");
+		System.out.println("Branch b andbranch b");
 		System.out.println("-------------------============");
 	}
 
